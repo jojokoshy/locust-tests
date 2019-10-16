a60c20a5d5a3b62e946d20297be2adb2889391cd
Vinicius Carvalho <vinnyc@vinnyc-macbookpro2.roam.corp.google.com>
2019-10-15 22:36:14 -0400

    simpler example

--- simple/example.py
+++ simple/example.py
@@ -1,25 +1,13 @@
 from locust import HttpLocust, TaskSet
 
-def login(l):
-    l.client.post("/login", {"username":"ellen_key", "password":"education"})
-
-def logout(l):
-    l.client.post("/logout", {"username":"ellen_key", "password":"education"})
 
 def index(l):
     l.client.get("/")
 
-def profile(l):
-    l.client.get("/profile")
 
 class UserBehavior(TaskSet):
-    tasks = {index: 2, profile: 1}
+    tasks = [index]
 
-    def on_start(self):
-        login(self)
-
-    def on_stop(self):
-        logout(self)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
